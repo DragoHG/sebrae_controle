afe6498a3c5ba963c9cb993ca4ea49686db03bae
Fabio Santana Monreal <t_fabiosm@ese.edu.br>
2019-09-02 16:36:49 -0300

    criado esboco no front e backend de troca de mes

--- eventos/urls.py
+++ eventos/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'eventos'
 urlpatterns = [
     #path('', views.index, name='index'),
-    path(r'eventos', views.CalendarView.as_view(), name='eventos'),
+    path(r'eventos', views.CalendarView.as_view(), name='calendar'),
+    path(r'eventos/new/', views.event, name='event_new'),
+    path(r'eventos/edit/(?P<event_id>\d+)/', views.event, name='event_edit'),
 ]
